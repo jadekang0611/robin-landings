fc56a40fcba9392c09ad0903426563550b1c6354
daisyy125 <daisy125@gmail.com>
2020-07-16 16:25:56 -0400

    update demobox js with content and styling

--- src/views/Outcomes/components/OutcomesOverview/components/DemoBox.js
+++ src/views/Outcomes/components/OutcomesOverview/components/DemoBox.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import '../OutcomesOverview.css';
+import { Jumbotron } from 'react-bootstrap';
 import demoRectangle from '../../../../../assets/rectangle.png';
 
 
 const DemoBox = () => {
+    let history = useHistory()
+
+    function handleClick() {
+        history.push('/request-demo')
+    }
 
     return (
-        <div>This is DemoBox section</div>
+        <Jumbotron className="demoMessage">
+            <h1>Ready to improve your skills and get the opportunity to be hired in top companies?</h1>
+            <button onClick={handleClick} className="demo-button">Request for Demo</button>
+        </Jumbotron>
     );
 };
 
