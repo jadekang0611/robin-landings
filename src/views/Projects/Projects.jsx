70e60fc5d3131f0f4182e127715856efc5a32827
daisyy125 <daisy125@gmail.com>
2020-09-17 09:57:27 -0400

    update background image for projects

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -48,13 +48,13 @@ const Projects = () => {
     };
 
     return (
-      <>
-        <div class='project-card'>
-          <div class='project-card-body'>
+      <div className='project-container'>
+        <div className='project-card'>
+          <div className='project-card-body'>
             <Row>
               <Col sm={5} className='project-title'>
-                <h5 class='project-card-title'>User Authentication Project</h5>
-                <h6 class='project-card-subtitle mb-2 text-muted'>
+                <h5 className='project-card-title'>User Authentication Project</h5>
+                <h6 className='project-card-subtitle mb-2 text-muted'>
                   What you will be building...
                 </h6>
               </Col>
@@ -72,7 +72,7 @@ const Projects = () => {
                 </Row>
               </Col>
             </Row>
-            <p class='project-card-text'>
+            <p className='project-card-text'>
               User Authentication might sound intimidating, but may be easier
               than you think. It's always the first page we see when we want to
               log into our favorite sites. Our challenge this week is to
@@ -80,15 +80,15 @@ const Projects = () => {
               Don't forget to add a bit of your personality to it!
             </p>
             <hr></hr>
-            <ul class='nav' id='project-nav'>
-              <li class={signStyle} id='signUp' onClick={handleSignClick}>
+            <ul className='nav' id='project-nav'>
+              <li className={signStyle} id='signUp' onClick={handleSignClick}>
                 Sign Up
               </li>
-              <li class={enterStyle} id='howTo' onClick={handleEnterClick}>
+              <li className={enterStyle} id='howTo' onClick={handleEnterClick}>
                 How To Enter
               </li>
               <li
-                class={resourceStyle}
+                className={resourceStyle}
                 id='resource'
                 onClick={handleResourceClick}>
                 Resources
@@ -98,7 +98,7 @@ const Projects = () => {
           </div>
         </div>
         <Footer />
-      </>
+      </div>
     );
 };
 
