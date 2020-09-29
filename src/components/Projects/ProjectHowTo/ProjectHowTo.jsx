11f51b143b97df188722ccd00521646896168a1d
taylorkang <taylor@myrobin.io>
2020-09-30 01:27:33 +0900

    ready for project launch

--- src/components/Projects/ProjectHowTo/ProjectHowTo.jsx
+++ src/components/Projects/ProjectHowTo/ProjectHowTo.jsx
@@ -1,82 +1,79 @@
 import React from 'react';
 import '../Projects.css';
-import {
-    Container,
-    Row,
-    Col,
-    Button,
-} from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const StepOne = '/images/step1.png';
 const StepTwo = '/images/step2.png';
 const StepThree = '/images/step3.png';
 
 const ProjectHowTo = () => {
-
-    return (
-      <Container className='steps-container'>
-        <Row className='steps' id='step-one'>
-          <Col className='number-container' xs={1}>
-            <img src={StepOne} alt='step-one'></img>
-          </Col>
-          <Col className='step-info-container' xs={9}>
-            <p>
-              Sign up by submitting your name and email before Project Challenge
-              start date.
-            </p>
-          </Col>
-        </Row>
-        <Row className='steps' id='step-two'>
-          <Col className='number-container' xs={1}>
-            <img src={StepTwo} alt='step-two'></img>
-          </Col>
-          <Col className='step-info-container' xs={9}>
-            <p>
-              Submit your final Project Challenge before deadline from your
-              Robin account. Project eligible submissions can be either Github,
-              CodePen or deployed URL.
-              <br></br>
-              <br></br>
-              <span id='free-account'>
-                Don't have a Robin account? Don't worry, you can create your
-                free one{' '}
-                <a
-                  href='https://app.myrobin.io/sign-up'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  here
-                </a>
-                !
-              </span>
-            </p>
-          </Col>
-        </Row>
-        <Row className='steps' id='step-three'>
-          <Col className='number-container' xs={1}>
-            <img src={StepThree} alt='step-three'></img>
-          </Col>
-          <Col className='step-info-container' xs={9}>
-            <p>
-              Robin will announce winners on{' '}
+  return (
+    <Container className='steps-container'>
+      <Row className='steps' id='step-one'>
+        <Col className='number-container' xs={1}>
+          <img src={StepOne} alt='step-one'></img>
+        </Col>
+        <Col className='step-info-container' xs={9}>
+          <p>
+            Sign up by submitting your name and email before Project Challenge
+            start date.
+          </p>
+        </Col>
+      </Row>
+      <Row className='steps' id='step-two'>
+        <Col className='number-container' xs={1}>
+          <img src={StepTwo} alt='step-two'></img>
+        </Col>
+        <Col className='step-info-container' xs={9}>
+          <p>
+            Submit your final Project Challenge before the deadline from your
+            Robin account. Project eligible submissions can be either Github,
+            CodePen or deployed URL.
+            <br></br>
+            <br></br>
+            <span id='free-account'>
+              Don't have a Robin account? Don't worry, you can create your free
+              one{' '}
               <a
-                href='https://myrobin.io/about-us'
+                href='https://app.myrobin.io/sign-up'
                 target='_blank'
-                rel='noopener noreferrer'>
-                Robin
-              </a>{' '}
-              and{' '}
-              <a
-                href='https://www.linkedin.com/company/joinrobin'
-                target='_blank'
-                rel='noopener noreferrer'>
-                LinkedIn
+                rel='noopener noreferrer'
+              >
+                here
               </a>
               !
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    );
+            </span>
+          </p>
+        </Col>
+      </Row>
+      <Row className='steps' id='step-three'>
+        <Col className='number-container' xs={1}>
+          <img src={StepThree} alt='step-three'></img>
+        </Col>
+        <Col className='step-info-container' xs={9}>
+          <p>
+            Robin will announce winners on{' '}
+            <a
+              href='https://myrobin.io/about-us'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Robin
+            </a>{' '}
+            and{' '}
+            <a
+              href='https://www.linkedin.com/company/joinrobin'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              LinkedIn
+            </a>
+            !
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProjectHowTo;
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,117 +1,116 @@
 import React, { useState } from 'react';
 import './Projects.css';
 import { Footer } from '../../components';
-import { ProjectHowTo, ProjectResource, ProjectSignUp } from '../../components/Projects';
+import {
+  ProjectHowTo,
+  ProjectResource,
+  ProjectSignUp,
+} from '../../components/Projects';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import copy from 'copy-to-clipboard';
 
 import ShareIcon from './ShareIcon.svg';
 
 const TABS = {
-    'signUp': <ProjectSignUp />,
-    'howTo': <ProjectHowTo />,
-    'resource': <ProjectResource />,
-}
-
+  signUp: <ProjectSignUp />,
+  howTo: <ProjectHowTo />,
+  resource: <ProjectResource />,
+};
 
 const Projects = () => {
-    const [ selectedTab, setSelectedTab ] = useState('signUp');
-    const [ signStyle, setSignStyle ] = useState('nav-click');
-    const [ enterStyle, setEnterStyle ] = useState ('nav-item');
-    const [ resourceStyle, setResourceStyle ] = useState('nav-item');
-    const [copySuccess, setCopySuccess] = useState(false);
+  const [selectedTab, setSelectedTab] = useState('signUp');
+  const [signStyle, setSignStyle] = useState('nav-click');
+  const [enterStyle, setEnterStyle] = useState('nav-item');
+  const [resourceStyle, setResourceStyle] = useState('nav-item');
+  const [copySuccess, setCopySuccess] = useState(false);
 
-    const handleSignClick = (e) => {
-        e.preventDefault();
-        console.log(e.target.id)
-        setSelectedTab(e.target.id);
-        setSignStyle('nav-click');
-        setEnterStyle('nav-item');
-        setResourceStyle('nav-item');
-    }
+  const handleSignClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    setSelectedTab(e.target.id);
+    setSignStyle('nav-click');
+    setEnterStyle('nav-item');
+    setResourceStyle('nav-item');
+  };
 
-    const handleEnterClick = (e) => {
-        e.preventDefault();
-        console.log(e.target.id);
-        setSelectedTab(e.target.id);
-        setEnterStyle('nav-click');
-        setSignStyle('nav-item');
-        setResourceStyle('nav-item');
-    };
+  const handleEnterClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    setSelectedTab(e.target.id);
+    setEnterStyle('nav-click');
+    setSignStyle('nav-item');
+    setResourceStyle('nav-item');
+  };
 
-    const handleResourceClick = (e) => {
-        e.preventDefault();
-        console.log(e.target.id);
-        setSelectedTab(e.target.id);
-        setResourceStyle('nav-click');
-        setSignStyle('nav-item');
-        setEnterStyle('nav-item');
-    };
+  const handleResourceClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    setSelectedTab(e.target.id);
+    setResourceStyle('nav-click');
+    setSignStyle('nav-item');
+    setEnterStyle('nav-item');
+  };
 
-    const copyToClipboard = () => {
-      let value = `https://myrobin.io/projects`;
-      setCopySuccess(true);
-    };
+  const copyToClipboard = () => {
+    let value = `https://myrobin.io/projects`;
+    copy(value);
+    setCopySuccess(true);
+  };
 
-    return (
-      <div className='project-container'>
-        <div className='project-card'>
-          <div className='project-card-body'>
-            <Row className='top-row'>
-              <Col sm={5} className='project-title'>
-                <h5 className='project-card-title'>
-                  User Authentication Project
-                </h5>
-                <h6 className='project-card-subtitle mb-2 text-muted'>
-                  What you will be building...
-                </h6>
-              </Col>
-              <Col sm={3} className='project-dates'>
-                <p>10/5/20 - 10/9/20</p>
-              </Col>
-              <Col sm={2} className='project-share-container'>
-                <Row className='projects-share'>
-                  <Col xs={12} onClick={copyToClipboard}>
-                    <img src={ShareIcon} alt='share' className='share' />
-                    <p>Share Profile</p>
-                  </Col>
-                </Row>
-                <Row className='projects-share'>
-                  <Col xs={12}>
-                    <i className='fas fa-envelope'></i>
-                    <i className='fab fa-linkedin'></i>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-            <p className='project-card-text'>
-              User Authentication might sound intimidating, but may be easier
-              than you think. It's always the first page we see when we want to
-              sign up or log into our favorite sites. Our challenge this week is
-              to recreate your favorite site sign up and login page with full
-              CRUD operations. Don't forget to add a bit of your personality to
-              it!
-            </p>
-            <hr></hr>
-            <ul className='nav' id='project-nav'>
-              <li className={signStyle} id='signUp' onClick={handleSignClick}>
-                Sign Up
-              </li>
-              <li className={enterStyle} id='howTo' onClick={handleEnterClick}>
-                How To Enter
-              </li>
-              <li
-                className={resourceStyle}
-                id='resource'
-                onClick={handleResourceClick}>
-                Resources
-              </li>
-            </ul>
-            {TABS[selectedTab]}
-          </div>
+  return (
+    <div className='project-container'>
+      <div className='project-card'>
+        <div className='project-card-body'>
+          <Row className='top-row'>
+            <Col sm={5} className='project-title'>
+              <h5 className='project-card-title'>
+                User Authentication Project
+              </h5>
+              <h6 className='project-card-subtitle mb-2 text-muted'>
+                What you will be building...
+              </h6>
+            </Col>
+            <Col sm={3} className='project-dates'>
+              <p>10/5/20 - 10/9/20</p>
+            </Col>
+            <Col sm={2} className='project-share-container'>
+              <Row className='projects-share'>
+                <Col xs={12} onClick={copyToClipboard}>
+                  <img src={ShareIcon} alt='share' className='share' />
+                  <p>Copy Link</p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <p className='project-card-text'>
+            User Authentication might sound intimidating, but may be easier than
+            you think. It's always the first page we see when we want to sign up
+            or log into our favorite sites. Our challenge this week is to
+            recreate your favorite site sign up and login page with full CRUD
+            operations. Don't forget to add a bit of your personality to it!
+          </p>
+          <hr></hr>
+          <ul className='nav' id='project-nav'>
+            <li className={signStyle} id='signUp' onClick={handleSignClick}>
+              Sign Up
+            </li>
+            <li className={enterStyle} id='howTo' onClick={handleEnterClick}>
+              How To Enter
+            </li>
+            <li
+              className={resourceStyle}
+              id='resource'
+              onClick={handleResourceClick}
+            >
+              Resources
+            </li>
+          </ul>
+          {TABS[selectedTab]}
         </div>
-        <Footer />
       </div>
-    );
+      <Footer />
+    </div>
+  );
 };
 
 export default Projects;
