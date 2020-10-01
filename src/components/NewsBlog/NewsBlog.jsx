188b08c79b96da6139d846707eb25b9b08d3878c
daisyy125 <daisy125@gmail.com>
2020-10-01 13:31:15 -0400

    update latest news blog with project

--- src/components/NewsBlog/NewsBlog.jsx
+++ src/components/NewsBlog/NewsBlog.jsx
@@ -5,63 +5,74 @@ import { Row, Col } from 'react-bootstrap';
 
 const NewsBlog = () => {
   return (
-    <div className="newsContainer">
+    <div className='newsContainer'>
       <h1>Latest News</h1>
-      <Row className="newsRow">
-        <Col xs={12} md={6} lg={4} className="news-content">
-          <div className="news-card1">
-            <h2 className="news-title">New Robin is coming!</h2>
-            <p className="news-description">
+      <Row className='newsRow'>
+        <Col xs={12} md={6} lg={4} className='news-content'>
+          <div className='news-card1'>
+            <h2 className='news-title'>New Robin has arrived!</h2>
+            <p className='news-description'>
               We heard needs and feedbacks from current and graduate bootcamp
-              students and outcomes. Stay tuned for a new Robin!
+              students and outcomes. Robin has a whole new look and we're
+              excited to show you what we've been working on.{' '}
+              <a
+                href='https://app.myrobin.io/sign-up'
+                target='_blank'
+                rel='noopener noreferrer'
+                alt='login'>
+                Sign up
+              </a>{' '}
+              or{' '}
+              <a
+                href='https://app.myrobin.io/signin'
+                target='_blank'
+                rel='noopener noreferrer'
+                alt='login'>
+                log in
+              </a>{' '}
+              to see what's new!
             </p>
             <div>
-            <img src="/images/blogs/NewLook.png" alt="article-img"></img>
+              <img src='/images/blogs/NewLook.png' alt='article-img'></img>
             </div>
           </div>
         </Col>
-        <Col xs={12} md={6} lg={4} className="news-content">
-          <div className="news-card2">
-            <h2 className="news-title">Live Event</h2>
-            <p className="news-description">
-                Are you considering bootcamp or concerned about your plan after
-                bootcamp? Hear from Jade Kang our co-founder at the live AMA
-                event hosted by CareerKarma.
+        <Col xs={12} md={6} lg={4} className='news-content'>
+          <div className='news-card2'>
+            <h2 className='news-title'>Robin 1st Project Week</h2>
+            <p className='news-description'>
+              Looking for a new project to build? Then get ready to join us for
+              our first project week 10/5 -10/9! During Robin project week, you
+              will be focused on building a feature component that will be
+              useful for your future projects.
             </p>
             <a
-                href="https://calendly.com/melvinsalvador/ck-ama-jade-kang/?month=2020-07"
-                target="_blank"
-                rel="noopener noreferrer"
-                alt="example title"
-            >
-              Read More <i class="fas fa-long-arrow-alt-right"></i>
+              href='/projects'
+              target='_blank'
+              rel='noopener noreferrer'
+              alt='projects'>
+              Read More <i class='fas fa-long-arrow-alt-right'></i>
             </a>
-            <img src="/images/blogs/AMA_Event.png" alt="article-img"></img>
+            <img src='/images/blogs/project-week-min.png' alt='article-img'></img>
           </div>
         </Col>
-        <Col xs={12} md={6} lg={4} className="news-content">
-          <div className="news-card3">
-            <h2 className="news-title">Welcome onboard</h2>
-            <p className="news-description">
+        <Col xs={12} md={6} lg={4} className='news-content'>
+          <div className='news-card3'>
+            <h2 className='news-title'>Welcome onboard</h2>
+            <p className='news-description'>
               Robin team is welcoming our team member no. 3! We are excited to
               work with our new Front-end Engineer, Daisy Yau.
-              </p>
+            </p>
             <a
-              href="https://www.linkedin.com/feed/update/urn:li:activity:6673225475286011904/"
-              target="_blank"
-              rel="noopener noreferrer"
-              alt="example title"
-            >
-              Read More <i class="fas fa-long-arrow-alt-right"></i>
+              href='https://www.linkedin.com/feed/update/urn:li:activity:6673225475286011904/'
+              target='_blank'
+              rel='noopener noreferrer'
+              alt='example title'>
+              Read More <i class='fas fa-long-arrow-alt-right'></i>
             </a>
-              <img
-                src="/images/blogs/Welcome_Daisy.png"
-                alt="article-img"
-              ></img>
+            <img src='/images/blogs/Welcome_Daisy.png' alt='article-img'></img>
           </div>
         </Col>
-
-
       </Row>
     </div>
   );
